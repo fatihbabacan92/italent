b53de8bc7bf1778eaabc8ea71bb7b447f3221e41
Fatih <fatihb@live.be>
2020-04-19 20:42:24 +0100

    Init Project

---
+++ app/src/main/java/tech/fatih/italent/MainActivity.kt
@@ -0,0 +1,62 @@
+package tech.fatih.italent
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import tech.fatih.italent.Activities.ActivitiesFragment
+import tech.fatih.italent.Home.HomeFragment
+import tech.fatih.italent.Reflection.ReflectionFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private var content: FrameLayout? = null
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                val fragment = HomeFragment.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_activities -> {
+                //message.setText(R.string.title_settings)
+                val fragment = ActivitiesFragment.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_reflection -> {
+                //message.setText(R.string.title_settings)
+                val fragment = ReflectionFragment.Companion.newInstance()
+                addFragment(fragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        content = findViewById(R.id.content)
+        val navigation: BottomNavigationView = findViewById(R.id.navigation)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val fragment = HomeFragment.Companion.newInstance()
+        addFragment(fragment)
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
+            .replace(R.id.content, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(fragment.javaClass.simpleName)
+            .commit()
+    }
+}
