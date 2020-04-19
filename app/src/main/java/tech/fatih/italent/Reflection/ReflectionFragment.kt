45d9275c1989ceb610f355cd3a74d781bd32e231
Fatih <fatihb@live.be>
2020-04-19 21:45:32 +0100

    Added ReflectionFragment

--- app/src/main/java/tech/fatih/italent/Reflection/ReflectionFragment.kt
+++ app/src/main/java/tech/fatih/italent/Reflection/ReflectionFragment.kt
@@ -3,10 +3,12 @@ package tech.fatih.italent.Reflection
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_reflection.view.*
 import tech.fatih.italent.Activities.ActivityDetailFragment
 import tech.fatih.italent.R
 
@@ -29,7 +31,8 @@ class ReflectionFragment : Fragment() {
         var rootView = inflater!!.inflate(R.layout.fragment_reflection, container, false)
         val context: Context? = super.getContext()
 
+        rootView.reflection_textview.movementMethod = ScrollingMovementMethod()
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return rootView
     }
 }
