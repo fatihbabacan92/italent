a71bb0353e1cb51bad94728ef00b6fb93d31c876
Fatih <fatihb@live.be>
2020-04-19 21:33:40 +0100

    Added HomeFragment

--- app/src/main/java/tech/fatih/italent/Home/HomeFragment.kt
+++ app/src/main/java/tech/fatih/italent/Home/HomeFragment.kt
@@ -3,10 +3,12 @@ package tech.fatih.italent.Home
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.view.*
 import tech.fatih.italent.R
 
 class HomeFragment : Fragment() {
@@ -28,7 +30,9 @@ class HomeFragment : Fragment() {
         var rootView = inflater!!.inflate(R.layout.fragment_home, container, false)
         val context: Context? = super.getContext()
 
+        rootView.introduction_textview.movementMethod = ScrollingMovementMethod()
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return rootView
     }
+
 }
